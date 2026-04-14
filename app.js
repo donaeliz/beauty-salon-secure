c0390fe180a11578a4b0b4ed7df5aa0c12f20c42
Dona Eliz <donaelizabeth7306@gmail.com>
2026-04-14 18:00:18 +0530

    fix: add Helmet.js security headers and secure session config

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/users/index');
 
@@ -13,18 +14,28 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// FIXED: Helmet adds security headers
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// VULNERABLE session config (intentional for assignment)
+// FIXED: Secure session configuration
 app.use(session({
-  secret: 'secrettobecchanged',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { httpOnly: false, secure: false }
+  secret: 'glow-beauty-salon-super-secret-key-2024',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+    maxAge: 1000 * 60 * 60
+  }
 }));
 
 app.use('/', indexRouter);
